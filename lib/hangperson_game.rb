93be3146e0174fc16031523f24a2c5649d717e9b
EC2 Default User <ec2-user@ip-172-31-9-230.eu-west-1.compute.internal>
2019-02-03 22:53:15 +0000

    Finished guess method

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -6,15 +6,41 @@ class HangpersonGame
   
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
-  
   def initialize(word)
-    @word = word
+    @word = word.downcase
     @guesses = ''
     @wrong_guesses = ''
   end
-
+  
+  def guess(letter)
+    # the letter cannot be null or a character other than letters from a-z A-Z
+    if letter == nil or not letter =~ /^[a-zA-Z]$/i or not String(letter)
+      raise ArgumentError 
+    end
+    
+    # change letters to downcase
+    letter.downcase!
+    
+=begin 
+  If the word has the letter and the letter hasn't been guessed before 
+    then add the letter to the guessed letters list.
+  If the word does not include the guessed letter and the letter hasn't been guessed before 
+    then add the letter to the wrong guessed letters list.
+=end
+    if word.include? letter and not @guesses.include? letter
+      @guesses.concat letter
+      return true
+    elsif not @word.include? letter and not @wrong_guesses.include? letter
+      @wrong_guesses.concat letter
+      return true
+    else
+      return false
+    end
+  end
+  
+  def word_with_guesses
+  end
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
